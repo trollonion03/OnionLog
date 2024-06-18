d7f2199aa231e2295a14701f5e882c89af56ed37
trollonion03 <trollonion03@gmail.com>
2024-06-18 14:50:32 +0000

    [Edit] khe - Add basic elements

--- src/Routes/MainPage.js
+++ src/Routes/MainPage.js
@@ -5,28 +5,40 @@ import './MainPage.css';
 
 const MainPage = () => {
     const isLargeScreen = useMediaQuery({minDeviceWidth: 1180});
+    const isLargeHeight = useMediaQuery({maxDeviceHeight: 899});
 
-    console.log(isLargeScreen);
+    const navStyle = {
+        maxWidth: isLargeScreen ? 'calc(1180px - 20px)' : '100%',
+    };
 
     const divStyle = {
         maxWidth: isLargeScreen ? '1180px' : '100%',
-        height: '77px',
-        margin: '0 auto'
     };
 
-    const bodyStyle = {
-        maxWidth: isLargeScreen ? '1180px' :'100%',
+    const cardStyle = {
+        maxWidth: isLargeScreen ? '1180px' : '100%',
+        height: isLargeHeight ? '720px' : 'inherit'
+    }
+
+    const onClickLogin = () => {
+        console.log('login');
     }
 
     return (
         <div className='MainPage'>
             <header>
-                <div className='NavBar' style={divStyle}>
-                    <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>OnionLog</Link>
+                <div className='NavBar' style={navStyle}>
+                    <div>
+                        <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>OnionLog</Link>
+                        <Link style={{ textDecoration: 'none', color: 'black', fontSize: '18px', marginLeft: '63px', fontFamily: 'inter-thin'}} to='blog'>블로그</Link>
+                    </div>
+                    <button type='submit' onClick={onClickLogin}>로그인</button>
                 </div>
             </header>
-            <div className='MainBody' style={bodyStyle}>
+            <div className='MainBody' style={divStyle}>
+                <div className='Card' style={cardStyle}>
 
+                </div>
             </div>
             <footer>
                 <div className='FooterBar' style={divStyle}>
