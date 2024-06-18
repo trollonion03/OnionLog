39cd8734d95d1d13aea97356b1e55356dba1775d
trollonion03 <trollonion03@gmail.com>
2024-06-18 11:22:35 +0000

    [Edit] khe - Add elements

--- src/Routes/MainPage.js
+++ src/Routes/MainPage.js
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import './MainPage.css';
 
 const MainPage = () => {
+    const isLargeScreen = useMediaQuery({minDeviceWidth: 1180});
+
+    console.log(isLargeScreen);
+
+    const divStyle = {
+        maxWidth: isLargeScreen ? '1180px' : '100%',
+        height: '77px',
+        margin: '0 auto'
+    };
+
+    const bodyStyle = {
+        maxWidth: isLargeScreen ? '1180px' :'100%',
+    }
+
     return (
         <div className='MainPage'>
             <header>
-                <h1>Hello World!</h1>
+                <div className='NavBar' style={divStyle}>
+                    <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>OnionLog</Link>
+                </div>
             </header>
-            <body>
+            <div className='MainBody' style={bodyStyle}>
 
-            </body>
+            </div>
             <footer>
+                <div className='FooterBar' style={divStyle}>
 
+                </div>
             </footer>
         </div>
     );
