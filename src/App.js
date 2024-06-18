3151383ec01b89882cc146dcd582bbe84d5b9455
trollonion03 <trollonion03@gmail.com>
2024-06-18 09:57:12 +0000

    [Init] khe - Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
---
+++ src/Routes/MainPage.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import './MainPage.css';
+
+const MainPage = () => {
+    return (
+        <div className='MainPage'>
+            <header>
+                <h1>Hello World!</h1>
+            </header>
+            <body>
+
+            </body>
+            <footer>
+
+            </footer>
+        </div>
+    );
+};
+
+export default MainPage;
