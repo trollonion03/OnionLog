fc895d5c094d94b3e82b2ae49e0ca4bd3eec4c3c
trollonion03 <trollonion03@gmail.com>
2024-06-19 05:53:28 +0000

    [Edit] khe - change elements position

--- src/Routes/MainPage.js
+++ src/Routes/MainPage.js
@@ -5,7 +5,7 @@ import './MainPage.css';
 
 const MainPage = () => {
     const isLargeScreen = useMediaQuery({minDeviceWidth: 1180});
-    const isLargeHeight = useMediaQuery({maxDeviceHeight: 899});
+    const isLargeHeight = useMediaQuery({minDeviceHeight: 899});
 
     const navStyle = {
         maxWidth: isLargeScreen ? 'calc(1180px - 20px)' : '100%',
@@ -15,10 +15,16 @@ const MainPage = () => {
         maxWidth: isLargeScreen ? '1180px' : '100%',
     };
 
-    const cardStyle = {
-        maxWidth: isLargeScreen ? '1180px' : '100%',
-        height: isLargeHeight ? '720px' : 'inherit'
+    const bodyStyle = {
+        maxWidth: isLargeScreen ? '1180px' : '100px',
+        height: isLargeHeight? '770px' : 'calc(100vh - 102px)' 
     }
+
+    const cardStyle = {
+        width: isLargeScreen ? '1180px' : '100%',
+        height: '720px'
+    }
+
 
     const onClickLogin = () => {
         console.log('login');
@@ -29,13 +35,13 @@ const MainPage = () => {
             <header>
                 <div className='NavBar' style={navStyle}>
                     <div>
-                        <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>OnionLog</Link>
-                        <Link style={{ textDecoration: 'none', color: 'black', fontSize: '18px', marginLeft: '63px', fontFamily: 'inter-thin'}} to='blog'>블로그</Link>
+                        <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>OnionLog_</Link>
+                        <Link style={{ textDecoration: 'none', color: 'black', fontSize: '18px', marginLeft: '63px' }} to='blog'>블로그</Link>
                     </div>
                     <button type='submit' onClick={onClickLogin}>로그인</button>
                 </div>
             </header>
-            <div className='MainBody' style={divStyle}>
+            <div className='MainBody' style={bodyStyle}>
                 <div className='Card' style={cardStyle}>
 
                 </div>
