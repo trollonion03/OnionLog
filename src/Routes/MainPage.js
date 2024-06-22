631f0fd961de79669760bc116563d373c0403faa
trollonion03 <trollonion03@gmail.com>
2024-06-22 13:34:37 +0000

    [Feat] khe - Change wheel event handling method

--- src/Routes/MainPage.js
+++ src/Routes/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 import './MainPage.css';
@@ -7,10 +7,13 @@ const MainPage = () => {
     const isLargeScreen = useMediaQuery({minDeviceWidth: 1180});
     const isLargeHeight = useMediaQuery({minDeviceHeight: 899});
 
-    //mouse scroll effect
+    //mouse scroll effect(refs)
     const cardRef = useRef(null);
-    const timerRef  = useRef(null);
-    const [scrollDist, setScrollDist] = useState(0);
+    const timerRef = useRef(null);
+    const wheelRef = useRef(0);
+    
+    //mouse scroll effect(constants)
+    const scrollThreshold = 500;
 
     const navStyle = {
         maxWidth: isLargeScreen ? 'calc(1180px - 20px)' : '100%',
@@ -35,43 +38,39 @@ const MainPage = () => {
         console.log('login');
     }
 
-    const handleWheel = (e) => {
-        console.log("called");
+    const handleWheel = useCallback((e) => {
         if(cardRef.current && cardRef.current.contains(e.target)) {
-            setScrollDist((prevDist) => {
-                var newDist = prevDist + e.deltaY;
+            console.log("called");
+            wheelRef.current += e.deltaY;
                 
-                if (newDist > 500) {
-                    newDist = 0;
-                    console.log("zero");
-                }
-                else if (newDist < 0) {
-                    newDist = 500;
-                    console.log("one");
-                }
+            if (wheelRef.current > scrollThreshold) {
+                wheelRef.current = 0;
+                console.log("zero");
+            }
+            else if (wheelRef.current < 0) {
+                wheelRef.current = scrollThreshold;
+                console.log("one");
+            }
                 
-                setScrollDist(newDist);
-                console.log(newDist);
-
-                return newDist;
-            });
+            console.log(wheelRef.current);
         }
-    };
+    }, []);
     
-    const debounceWheelEvent = (e) => {
+    const debounceWheelEvent = useCallback((e) => {
         if(cardRef.current && cardRef.current.contains(e.target)) {
             e.preventDefault();
             e.stopPropagation();
+            console.log(Boolean(timerRef.current));
         }
 
-        console.log(Boolean(timerRef.current));
+        
         if (!timerRef.current) {
             timerRef.current = setTimeout (() => {
                 timerRef.current = null;
                 handleWheel(e);
-            }, 2500);
+            }, 100);
         }
-    };
+    }, [handleWheel]);
 
     useEffect(() => {
         window.addEventListener('wheel', debounceWheelEvent, {passive: false});
@@ -83,7 +82,7 @@ const MainPage = () => {
                 timerRef.current = null;
             }
         };
-    }, []);
+    }, [debounceWheelEvent]);
 
     return (
         <div className='MainPage'>
@@ -98,6 +97,7 @@ const MainPage = () => {
             </header>
             <div className='MainBody' style={bodyStyle}>
                 <div className='Card' style={cardStyle} ref={cardRef}>
+                    
                 </div>
             </div>
             <footer>
