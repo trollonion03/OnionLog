21864a014742f22d79c2ddafcf66c488300ac0e6
trollonion03 <trollonion03@gmail.com>
2024-08-03 23:27:12 +0900

    [Edit] khe - Edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-cp -rfv ./build/* /home/ubuntu/trollonion03.github.io/
+current_date=$(date +"%y%m%d")
+pwd
+npm run build || exit 1
+cp -rfv ./build/* /home/ubuntu/trollonion03.github.io/ || exit 1
+cd /home/ubuntu/trollonion03.github.io/ || exit 1
+pwd
+git pull || exit 1
+git add . || exit 1
+git commit -m "[Build] khe - $current_date build" || exit 1
+git push || exit 1
