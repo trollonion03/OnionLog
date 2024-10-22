cad8fa27c294a73f29ed4187636bbca966f39725
trollonion03 <trollonion03@gmail.com>
2024-10-22 23:54:03 +0900

    [Chore] khe - Add axios package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import axios from "axios";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+axios.defaults.baseURL = "https://localhost:8000";
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
